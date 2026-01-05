e7f1b81637b197129a7648f37b7aff432225d5f2
Akshaj1717 <akshaj17@gmail.com>
2026-01-04 21:15:52 -0500

    Stack

---
+++ Stack/Main.java
@@ -0,0 +1,25 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Stack<String> stack = new Stack<>();
+        //System.out.println(stack.empty());
+
+        stack.push("Minecraft");
+        stack.push("Skyrim");
+        stack.push("DOOM");
+        stack.push("BorderLands");
+        stack.push("FFVII");
+
+        //System.out.println(stack.peek());
+        //System.out.println(stack.search("Minecraft"));
+       
+
+        //System.out.println(stack);
+
+    }
+}
