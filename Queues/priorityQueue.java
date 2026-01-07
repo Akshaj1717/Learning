ffb48f8ecd9e298fedfdb3b57eb65ffedeb54a03
Akshaj1717 <akshaj17@gmail.com>
2026-01-06 19:41:30 -0500

    main files

---
+++ Queues/priorityQueue.java
@@ -0,0 +1,21 @@
+package Queues;
+import java.util.*;
+
+public class priorityQueue 
+{
+    public static void main(String[] args)
+    {
+        Queue<String> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        queue.offer("B");
+        queue.offer("C");
+        queue.offer("A");
+        queue.offer("F");
+        queue.offer("D");
+
+        while(!queue.isEmpty())
+        {
+            System.out.println(queue.poll());
+        }
+    }
+}
