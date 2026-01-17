e5bfbd3e160913eaeb4bd869d86028ac3d0593fa
Akshaj1717 <akshaj17@gmail.com>
2026-01-16 20:06:03 -0500

    HashTables

---
+++ HashTables/Main.java
@@ -0,0 +1,24 @@
+package HashTables;
+
+import java.util.Hashtable;
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Hashtable<String, String> table = new Hashtable<>();
+
+        table.put("100", "Spongebob");
+        table.put("123", "Patrick");
+        table.put("321", "Sandy");
+        table.put("555", "Squidward");
+        table.put("777", "Gary");
+
+        //table.remove(777);
+
+        for (String key : table.keySet())
+        {
+            System.out.println(key.hashCode() % 21 + "\t" + key + "\t" + table.get(key));
+        }
+    }
+}
