26d39dd949ea2fc7d1f614cb659aa6059447ca17
Akshaj1717 <akshaj17@gmail.com>
2026-01-19 16:22:32 -0500

    ad

--- AdjacenyList/Graph.java
+++ AdjacenyList/Graph.java
@@ -1,5 +1,33 @@
 package AdjacenyList;
 
-public class Graph {
-    
+import java.util.ArrayList;
+
+public class Graph 
+{
+    ArrayList<LinkedList<Node>> alist;
+
+    Graph()
+    {
+        alist = new ArrayList<>();
+    }
+
+    public void addNode(Node node)
+    {
+
+    }
+
+    public void addEdge(int src, int dst)
+    {
+        
+    }
+
+    public void checkEdge(int src, int dst)
+    {
+        
+    }
+
+    public void print()
+    {
+        
+    }
 }
--- AdjacenyList/Main.java
+++ AdjacenyList/Main.java
@@ -4,6 +4,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        Graph graph = new Graph();
+
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
     }
 }
